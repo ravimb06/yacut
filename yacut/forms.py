94525c78c0aeaad5683b93b3fbb087146047debf
Ali Bogatyrev <marksheyder06@gmail.com>
2022-12-21 15:37:01 +0300

    created URLMapForm

---
+++ yacut/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, Length, Optional
+
+
+class URLMapForm(FlaskForm):
+    original = URLField(
+        'Введите исходную ссылку',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    short = StringField(
+        'Ваш вариант короткой ссылки',
+        validators=[Length(1, 16), Optional]
+    )
+    submit = SubmitField('Создать')
