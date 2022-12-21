c8fdaa32209405d8a4d6b1ea23ea3583a792613c
Ali Bogatyrev <marksheyder06@gmail.com>
2022-12-21 15:26:04 +0300

    created URLMap model

--- yacut/models.py
+++ yacut/models.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
-from yacut import db
+from . import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column(db.String(256), nullable=False)
+    short = db.Column(db.String(16), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
