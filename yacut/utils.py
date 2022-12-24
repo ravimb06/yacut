216419b63e750e25aa5e8a72f5ce2bd2fd15ddfd
Ali Bogatyrev <marksheyder06@gmail.com>
2022-12-24 16:47:07 +0300

    edited char_validator

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,6 +1,6 @@
-from random import randrange, choice
-from string import ascii_letters
 import re
+from random import choice, randrange
+from string import ascii_letters
 
 
 def get_unique_short_id():
@@ -11,6 +11,6 @@ def get_unique_short_id():
 
 
 def char_validator(text):
-    if len(text) > 16:
+    if not len(text) <= 16:
         return False
     return bool(re.match("""^[a-zA-Z0-9]+$""", text))
