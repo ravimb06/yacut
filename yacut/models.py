afae91178d336bc1af30b220dd21e337f10df172
Ali Bogatyrev <marksheyder06@gmail.com>
2022-12-24 16:46:28 +0300

    edited URLMap model

--- yacut/models.py
+++ yacut/models.py
@@ -6,19 +6,17 @@ from . import db
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(256), nullable=False)
-    short = db.Column(db.String(16), nullable=False)
+    short = db.Column(db.String(16))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def to_dict(self):
         return dict(
-            id = self.id,
-            original = self.original,
-            short = self.short,
-            timestamp = self.timestamp
+            short_link='http://localhost/' + self.short,
+            url=self.original
         )
 
-    
     def from_dict(self, data):
-        for field in ['original', 'short']:
-            if field in data:
-                setattr(self, field, data[field])
+        columns_dict = {'original': 'url', 'short': 'custom_id'}
+        for field in columns_dict.items():
+            if field[1] in data:
+                setattr(self, field[0], data[field[1]])
