f5cbde61b48aeffd59e64e84467c3c7554d361ad
Ali Bogatyrev <marksheyder06@gmail.com>
2022-12-25 19:45:01 +0300

    added regexp validation for URLMapForm

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, Regexp
 
 
 class URLMapForm(FlaskForm):
@@ -10,6 +10,10 @@ class URLMapForm(FlaskForm):
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
-        validators=[Length(1, 16), Optional()]
+        validators=[
+            Regexp('^[a-zA-Z0-9]+$', message='Недопустимые символы'),
+            Length(1, 16),
+            Optional()
+        ]
     )
     submit = SubmitField('Создать')
